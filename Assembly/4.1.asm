65c3017a176e21ff085520425c8becea2298ff9b
jagadeshkumarS <jagadeshs@univisiontechnocon.com>
2020-09-14 12:59:23 +0530

    Only Data sheets in Datasheets branch

--- Assembly/4.1.asm
+++
@@ -1,6 +0,0 @@
-cseg at 0
-	mov r0,#00h
-	rep:
-	inc r0
-	cjne r0,#0E1h,rep
-	end
--- Assembly/led.asm
+++
@@ -1,8 +0,0 @@
-extrn code(delay_1ms,delay_100ms,delay_500ms,delay_1s)
-	main:
-	clr p1.0
-	acall delay_500ms
-	setb p1.0
-	acall delay_500ms
-	sjmp main
-	end
